b57bfec8bd846e42032a6c23577a4c606c275e6f
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-01-06 21:36:50 -0300

    file type validator

--- index.js
+++ index.js
@@ -2,237 +2,255 @@
 const clock_module = {};
 
 // Start Module
-clock_module.new = function (data) {
+clock_module.new = function (data = {}) {
 
     try {
 
-        // Prepare Functions
-        const functions = require('firebase-functions');
+        // Exist Data
+        const objType = require('puddy-lib/get/objType');
+        if (objType(data, 'object')) {
 
-        // Lodash Module
-        const _ = require('lodash');
+            // Prepare Functions
+            const functions = require('firebase-functions');
 
-        // Create Settings
-        const tinyCfg = _.defaultsDeep({}, data.firebase, {
-            options: {
-                id: "main",
-                autoStart: {
-                    database: true
+            // Lodash Module
+            const _ = require('lodash');
+
+            // Create Settings
+            const tinyCfg = _.defaultsDeep({}, data.firebase, {
+                options: {
+                    id: "main",
+                    autoStart: {
+                        database: true
+                    }
                 }
+            });
+
+            // Create Clock Settings
+            const clockCfg = _.defaultsDeep({}, data.clock, {
+                id: 'clock',
+                schedule: 'every 1 minutes',
+                ref: ''
+            });
+
+            // Custom Module Config
+            if (!Array.isArray(data.modules)) {
+                data.modules = [];
             }
-        });
 
-        // Create Clock Settings
-        const clockCfg = _.defaultsDeep({}, data.clock, {
-            id: 'clock',
-            schedule: 'every 1 minutes',
-            ref: ''
-        });
+            // Exist Ref
+            let existRef = (typeof clockCfg.ref === "string" && clockCfg.ref.length > 0);
 
-        // Custom Module Config
-        if (!Array.isArray(data.modules)) {
-            data.modules = [];
-        }
+            // Start Firebase
+            const firebase = require('puddy-lib/firebase');
+            firebase.start(require('firebase-admin'), tinyCfg.options, tinyCfg.firebase);
 
-        // Exist Ref
-        let existRef = (typeof clockCfg.ref === "string" && clockCfg.ref.length > 0);
+            // App
+            const app = firebase.get(tinyCfg.options.id);
+            const moment = require('moment-timezone');
+            const clone = require('clone');
+            const arrayUniq = require('array-uniq');
 
-        // Start Firebase
-        const firebase = require('puddy-lib/firebase');
-        firebase.start(require('firebase-admin'), tinyCfg.options, tinyCfg.firebase);
+            // For Promise
+            const forPromise = require('for-promise');
 
-        // App
-        const app = firebase.get(tinyCfg.options.id);
-        const moment = require('moment-timezone');
-        const clone = require('clone');
-        const arrayUniq = require('array-uniq');
+            // Prepare Custom Module
+            const custom_module_manager = require('puddy-lib/libs/custom_module_loader');
 
-        // For Promise
-        const forPromise = require('for-promise');
+            // Nothing Ref
+            let db = null;
+            if (!existRef) {
 
-        // Prepare Custom Module
-        const custom_module_manager = require('puddy-lib/libs/custom_module_loader');
+                // DB
+                db = {
 
-        // Nothing Ref
-        let db = null;
-        if (!existRef) {
+                    // Module
+                    module: app.db.ref('module'),
 
-            // DB
-            db = {
+                    // Weeks In Year
+                    weeksInYear: app.db.ref('weeksInYear'),
 
-                // Module
-                module: app.db.ref('module'),
+                    // Clock
+                    clock: app.db.ref('clock')
+
+                };
+
+            }
+
+            // Exist
+            else {
+
+                // DB
+                db = {
+
+                    // Module
+                    module: app.db.ref(clockCfg.ref).child('module'),
+
+                    // Weeks In Year
+                    weeksInYear: app.db.ref(clockCfg.ref).child('weeksInYear'),
+
+                    // Clock
+                    clock: app.db.ref(clockCfg.ref).child('clock')
+
+                };
+
+            }
+
+            // Function
+            const tinyClock = async () => {
+
+                // Custom Module Items
+                const custom_module_options = { db: db, tz: {} };
+
+                // Get Timezone List
+                const timezone_list = moment.tz.names();
+                const timezone = { clock: {} };
+                const now = moment();
+
+                // Insert Custom Data
+                custom_module_options.now = now;
+                custom_module_options.timezone_list = timezone_list;
+
+                // Module Name
+                timezone.module = 'moment-timezone';
+                custom_module_options.module = timezone.module;
+                db.module.set(timezone.module);
 
                 // Weeks In Year
-                weeksInYear: app.db.ref('weeksInYear'),
+                timezone.weeksInYear = now.weeksInYear();
+                custom_module_options.weeksInYear = timezone.weeksInYear;
+                db.weeksInYear.set(timezone.weeksInYear);
 
-                // Clock
-                clock: app.db.ref('clock')
+                // Get All Times
+                await forPromise(timezone_list, function (item, fn, fn_error) {
 
-            };
+                    if (typeof timezone_list[item] === "string") {
 
-        }
+                        // Prepare Database
+                        const db_tz = db.clock.child(firebase.databaseEscape(timezone_list[item], true));
 
-        // Exist
-        else {
+                        // New Now
+                        const now_timezone = now.tz(timezone_list[item]);
 
-            // DB
-            db = {
+                        // Create Zone
+                        const zone = clone(moment.tz.zone(timezone_list[item]));
+                        delete zone.untils;
 
-                // Module
-                module: app.db.ref(clockCfg.ref).child('module'),
+                        // Anti Duplicate
+                        zone.abbrs = arrayUniq(zone.abbrs);
+                        zone.offsets = arrayUniq(zone.offsets);
 
-                // Weeks In Year
-                weeksInYear: app.db.ref(clockCfg.ref).child('weeksInYear'),
+                        // New Object
+                        timezone.clock[timezone_list[item]] = {
 
-                // Clock
-                clock: app.db.ref(clockCfg.ref).child('clock')
+                            // Abbr
+                            zone: clone(zone),
 
-            };
+                            // Module
+                            module: timezone.module,
 
-        }
+                            // Hour
+                            hour: now_timezone.hour(),
 
-        // Function
-        const tinyClock = async () => {
+                            // Hour
+                            minute: now_timezone.minute(),
 
-            // Custom Module Items
-            const custom_module_options = { db: db, tz: {} };
+                            // Day
+                            day: now_timezone.date(),
 
-            // Get Timezone List
-            const timezone_list = moment.tz.names();
-            const timezone = { clock: {} };
-            const now = moment();
+                            // Month
+                            month: now_timezone.month(),
 
-            // Insert Custom Data
-            custom_module_options.now = now;
-            custom_module_options.timezone_list = timezone_list;
-
-            // Module Name
-            timezone.module = 'moment-timezone';
-            custom_module_options.module = timezone.module;
-            db.module.set(timezone.module);
-
-            // Weeks In Year
-            timezone.weeksInYear = now.weeksInYear();
-            custom_module_options.weeksInYear = timezone.weeksInYear;
-            db.weeksInYear.set(timezone.weeksInYear);
-
-            // Get All Times
-            await forPromise(timezone_list, function (item, fn, fn_error) {
-
-                if (typeof timezone_list[item] === "string") {
-
-                    // Prepare Database
-                    const db_tz = db.clock.child(firebase.databaseEscape(timezone_list[item], true));
-
-                    // New Now
-                    const now_timezone = now.tz(timezone_list[item]);
-
-                    // Create Zone
-                    const zone = clone(moment.tz.zone(timezone_list[item]));
-                    delete zone.untils;
-
-                    // Anti Duplicate
-                    zone.abbrs = arrayUniq(zone.abbrs);
-                    zone.offsets = arrayUniq(zone.offsets);
-
-                    // New Object
-                    timezone.clock[timezone_list[item]] = {
-
-                        // Abbr
-                        zone: clone(zone),
-
-                        // Module
-                        module: timezone.module,
-
-                        // Hour
-                        hour: now_timezone.hour(),
-
-                        // Hour
-                        minute: now_timezone.minute(),
-
-                        // Day
-                        day: now_timezone.date(),
-
-                        // Month
-                        month: now_timezone.month(),
-
-                        // Month
-                        year: now_timezone.year(),
-
-                        // Week Day
-                        weekDay: now_timezone.day(),
-
-                        // Year Day
-                        yearDay: now_timezone.dayOfYear(),
-
-                        // Week Year
-                        weekYear: now_timezone.week(),
-
-                        // Locale Data
-                        locale: {
+                            // Month
+                            year: now_timezone.year(),
 
                             // Week Day
-                            weekDay: now_timezone.weekday(),
+                            weekDay: now_timezone.day(),
+
+                            // Year Day
+                            yearDay: now_timezone.dayOfYear(),
 
                             // Week Year
-                            weekYear: now_timezone.weekYear(),
+                            weekYear: now_timezone.week(),
 
-                        }
+                            // Locale Data
+                            locale: {
 
-                    };
+                                // Week Day
+                                weekDay: now_timezone.weekday(),
 
-                    // Set Timezone Data
-                    db_tz.set(timezone.clock[timezone_list[item]]).then(() => { fn(); }).catch(err => { fn_error(err); });
+                                // Week Year
+                                weekYear: now_timezone.weekYear(),
 
-                    // Insert Custom Data
-                    custom_module_options.tz[timezone_list[item]] = {
-                        db: db_tz,
-                        data: timezone.clock[timezone_list[item]]
-                    };
+                            }
 
-                }
+                        };
 
-                // Nope
-                else { fn(); }
+                        // Set Timezone Data
+                        db_tz.set(timezone.clock[timezone_list[item]]).then(() => { fn(); }).catch(err => { fn_error(err); });
+
+                        // Insert Custom Data
+                        custom_module_options.tz[timezone_list[item]] = {
+                            db: db_tz,
+                            data: timezone.clock[timezone_list[item]]
+                        };
+
+                    }
+
+                    // Nope
+                    else { fn(); }
+
+                    // Complete
+                    return;
+
+                });
+
+                // Send Custom Module
+                await custom_module_manager.run(data.modules, custom_module_options, 'clockUpdate');;
 
                 // Complete
                 return;
 
-            });
+            };
 
-            // Send Custom Module
-            await custom_module_manager.run(data.modules, custom_module_options, 'clockUpdate');;
+            // Results
+            const results = {};
 
-            // Complete
-            return;
+            // Prepare Result
+            if (typeof clockCfg.id === "string" && clockCfg.id.length > 0 && typeof clockCfg.schedule === "string" && clockCfg.schedule.length > 0) {
 
-        };
+                // Tester URL
+                results[clockCfg.id + 'Test'] = functions.https.onRequest(async (req, res) => {
 
-        // Results
-        const results = {};
+                    // Make the Test
+                    await tinyClock();
 
-        // Prepare Result
-        if (typeof clockCfg.id === "string" && clockCfg.id.length > 0 && typeof clockCfg.schedule === "string" && clockCfg.schedule.length > 0) {
+                    // Complete Request
+                    return res.send('Clock Test Complete!');
 
-            // Tester URL
-            results[clockCfg.id + 'Test'] = functions.https.onRequest(async (req, res) => {
+                });
 
-                // Make the Test
-                await tinyClock();
+                // Main
+                results[clockCfg.id] = functions.pubsub.schedule(clockCfg.schedule).onRun(tinyClock);
 
-                // Complete Request
-                return res.send('Clock Test Complete!');
+            }
 
-            });
-
-            // Main
-            results[clockCfg.id] = functions.pubsub.schedule(clockCfg.schedule).onRun(tinyClock);
+            // Return
+            return results;
 
         }
 
-        // Return
-        return results;
+        // Nope
+        else {
+
+            // Error
+            const err = new Error('Start Data Undefined!');
+
+            console.log(err);
+            console.log(err.message);
+            return null;
+
+        }
 
     }
 
